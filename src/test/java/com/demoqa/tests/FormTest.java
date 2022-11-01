5cc33227d5cf826a99dd9016c790940284a2b4f7
Xenia Bogatko <bogatkok@gmail.com>
2022-11-01 23:39:57 +0100

    selenide added

--- src/test/java/com/demoqa/tests/FormTest.java
+++ src/test/java/com/demoqa/tests/FormTest.java
@@ -15,7 +15,8 @@ public class FormTest {
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1024x768";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
