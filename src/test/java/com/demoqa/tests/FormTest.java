21eb7cce4894c5e5932f5a347f8f73cc2ccb001e
Xenia Bogatko <bogatkok@gmail.com>
2022-11-02 00:05:45 +0100

    Attach added

--- src/test/java/com/demoqa/tests/FormTest.java
+++ src/test/java/com/demoqa/tests/FormTest.java
@@ -3,7 +3,9 @@ package com.demoqa.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.pages.FormPage;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +22,13 @@ public class FormTest {
         Configuration.browserSize = "1024x768";
         //Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        //Attach.addVideo();
     }
 
     @Test
