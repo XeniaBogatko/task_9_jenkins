bf99f116f6c2fcb41ee850c9be3c483a2496c18d
Xenia Bogatko <bogatkok@gmail.com>
2022-11-01 23:44:32 +0100

    listener added

--- src/test/java/com/demoqa/tests/FormTest.java
+++ src/test/java/com/demoqa/tests/FormTest.java
@@ -1,7 +1,9 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.pages.FormPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,7 @@ public class FormTest {
 
     @BeforeAll
     static void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1024x768";
         //Configuration.holdBrowserOpen = true;
